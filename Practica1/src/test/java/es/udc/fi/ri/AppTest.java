aeccba39af52716d427ee90d9d599eb937f08d86
AbelJuncal <abel.juncal@udc.es>
2022-03-15 18:43:09 +0100

    Repositorio creado

---
+++ Practica1/src/test/java/es/udc/fi/ri/AppTest.java
@@ -0,0 +1,20 @@
+package es.udc.fi.ri;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
